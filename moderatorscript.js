84181e207cea8ca944ef3f3b5118bda993fec6be
EverythingTech <lyf19990329@me.com>
2016-06-02 17:20:15 -0700

    added change property in lb, delete checkbox

--- moderatorscript.js
+++ moderatorscript.js
@@ -18,6 +18,11 @@ function unHideTwo(divID1, divID2, newSrc){
 		
 		for(var i = 0; i < filename.length; i++){
 			if(filename[i] == currentShowing){
+				document.getElementById("lbfilename").setAttribute("value", ((filename[i]).split('/'))[1]);
+				document.getElementById("lbfirstname").setAttribute("value", firstname[i]);
+				document.getElementById("lblastname").setAttribute("value", lastname[i]);
+				document.getElementById("lbdescription").setAttribute("value", description[i]);
+				document.getElementById("lbtags").setAttribute("value", tags[i]);
 				document.getElementById("lb_download_url").setAttribute("href", filename[i]);	
 				document.getElementById("lb_download_url").setAttribute("download", newSrc);	
 				document.getElementById("imageFile").setAttribute("src", filename[i]);		
@@ -54,12 +59,23 @@ function goNext(){
 	for(var i = 0; i < filename.length; i++){
 		if(filename[i] == currentShowing){
 			if((i+1) >= filename.length){
+				document.getElementById("lbfilename").setAttribute("value", ((filename[0]).split('/'))[1]);
+				document.getElementById("lbfirstname").setAttribute("value", firstname[0]);
+				document.getElementById("lblastname").setAttribute("value", lastname[0]);
+				document.getElementById("lbdescription").setAttribute("value", description[0]);
+				document.getElementById("lbtags").setAttribute("value", tags[0]);
+			
 				document.getElementById("lb_download_url").setAttribute("href", filename[0]);
 				document.getElementById("lb_download_url").setAttribute("download", ((filename[0]).split('/'))[1]);		
 				document.getElementById("imageFile").setAttribute("src", filename[0]);
 				document.getElementById("basic_info").innerHTML = "<pre id = 'info'>Photographer: " + lastname[0]+"     " + "Description: " + description[0]+"</pre>";
 			}
 			else{
+				document.getElementById("lbfilename").setAttribute("value", ((filename[i+1]).split('/'))[1]);
+				document.getElementById("lbfirstname").setAttribute("value", firstname[i+1]);
+				document.getElementById("lblastname").setAttribute("value", lastname[i+1]);
+				document.getElementById("lbdescription").setAttribute("value", description[i+1]);
+				document.getElementById("lbtags").setAttribute("value", tags[i+1]);
 				document.getElementById("lb_download_url").setAttribute("href", filename[i+1]);
 				document.getElementById("lb_download_url").setAttribute("download", ((filename[i+1]).split('/'))[1]);	
 				document.getElementById("imageFile").setAttribute("src", filename[i+1]);
@@ -75,11 +91,21 @@ function goBack(){
 	for(var i = 0; i < filename.length; i++){
 		if(filename[i] == currentShowing){
 			if((i-1) < 0){
+				document.getElementById("lbfilename").setAttribute("value", ((filename[filename.length-1]).split('/'))[1]);
+				document.getElementById("lbfirstname").setAttribute("value", firstname[filename.length-1]);
+				document.getElementById("lblastname").setAttribute("value", lastname[filename.length-1]);
+				document.getElementById("lbdescription").setAttribute("value", description[filename.length-1]);
+				document.getElementById("lbtags").setAttribute("value", tags[filename.length-1]);
 				document.getElementById("lb_download_url").setAttribute("href", filename[filename.length-1]);
 				document.getElementById("lb_download_url").setAttribute("download", (filename[filename.length-1]).split('/')[1]);
 				document.getElementById("imageFile").setAttribute("src", filename[filename.length-1]);	
 				document.getElementById("basic_info").innerHTML = "<pre id = 'info'>Photographer: " + lastname[filename.length-1]+"     " + "Description: " + description[filename.length-1]+"</pre>";			
 			}else{
+				document.getElementById("lbfilename").setAttribute("value", ((filename[i-1]).split('/'))[1]);
+				document.getElementById("lbfirstname").setAttribute("value", firstname[i-1]);
+				document.getElementById("lblastname").setAttribute("value", lastname[i-1]);
+				document.getElementById("lbdescription").setAttribute("value", description[i-1]);
+				document.getElementById("lbtags").setAttribute("value", tags[i-1]);
 				document.getElementById("lb_download_url").setAttribute("href", filename[i-1]);
 				document.getElementById("lb_download_url").setAttribute("download", filename[i-1].split('/')[1]);
 				document.getElementById("imageFile").setAttribute("src", filename[i-1]);
@@ -91,7 +117,6 @@ function goBack(){
 }
 function addToDelete(src){
 	var formContents = document.getElementById('delete-selected-form').innerHTML;
-
 	if(document.getElementById(src).style.opacity == '0.4'){
 		var id = "remove"+src;
 		var child = document.getElementById(id);
@@ -106,5 +131,13 @@ function addToDelete(src){
 		formContents += newLine;
 		document.getElementById('delete-selected-form').innerHTML = formContents;
 		document.getElementById(src).style.opacity = '0.4';
+	}
+
+	if(deleteFormIndex == 0){
+		document.getElementById("delete-selected-button").style.opacity = "0.3";
+		document.getElementById("delete-selected-button").disabled = true;
+	}else{
+		document.getElementById("delete-selected-button").style.opacity = "1";
+		document.getElementById("delete-selected-button").disabled = false;
 	}
 }
